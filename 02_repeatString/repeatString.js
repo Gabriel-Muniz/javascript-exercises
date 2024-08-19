8f194a413f005a75c3171e891f3ae225695bb4e7
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-08-19 11:19:34 -0300

    Finish: repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let aux = "";
+  if (num == 0) return aux;
+  if (num < 0) {
+    aux = "ERROR"
+    return aux;
+  };
 
+  for (let i = 0; i < num; i++) {
+    aux += string;
+  }
+  return aux;
 };
 
 // Do not edit below this line
