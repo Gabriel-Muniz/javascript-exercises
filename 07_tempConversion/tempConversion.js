006efe1a22a3c8fcff23e3555aeabdc8db3132cf
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-08-20 19:43:56 -0300

    Finish: tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperaturaFahrenheit) {
+  let temperaturaConvertida = (temperaturaFahrenheit - 32) * 5/9;
+  return (temperaturaConvertida % 2 == 0) ? +temperaturaConvertida : +temperaturaConvertida.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperaturaCelsius) {
+  let temperaturaConvertida = temperaturaCelsius * 9/5 + 32;
+  return (temperaturaConvertida % 2 == 0) ? +temperaturaConvertida : +temperaturaConvertida.toFixed(1);
 };
 
 // Do not edit below this line
