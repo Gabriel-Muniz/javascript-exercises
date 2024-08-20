2b0fb6ee847aea47f2689b0037fb1ab8452558c2
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-08-19 22:24:15 -0300

    Finish: 4 tests sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (...numbers) {
+  let resultado = 0;
 
+  numbers.sort((a, b) => a - b);
+  
+  numeroInicial = numbers[0];
+  numeroFinal = numbers[1];
+
+  if (numbers.find((item) => item < 0)) return "ERROR";
+
+  for (let i = numeroInicial; i <= numeroFinal; i++) {
+    resultado += i;
+  }
+  return resultado;
 };
 
 // Do not edit below this line
