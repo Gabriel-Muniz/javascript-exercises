1e9f944e7294a6fef7f0af36585e0a0e278047eb
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-08-19 22:03:48 -0300

    Refactor: simplify code based on the solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,11 @@
 const removeFromArray = function ([...arrayOriginal], ...elementosRetirados) {
   //Checar se o arrayOriginal contem algum dos elementos a serem retirados
   let novoArray = [];
+  
+  arrayOriginal.forEach(item => {
+    if (!elementosRetirados.includes(item)) novoArray.push(item);
+  })
 
-  arrayFora: for (let i = 0; i < arrayOriginal.length; i++) {
-    for (let j = 0; j < elementosRetirados.length; j++) {
-      if (arrayOriginal[i] === elementosRetirados[j]) continue arrayFora;
-    }
-    novoArray.push(arrayOriginal[i]);
-  }
   return novoArray;
 };
 
