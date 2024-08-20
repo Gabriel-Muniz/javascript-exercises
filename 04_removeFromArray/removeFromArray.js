640341718ac06d063b985c30b3ee35e4146340c8
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-08-19 21:59:34 -0300

    Finish: removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function ([...arrayOriginal], ...elementosRetirados) {
+  //Checar se o arrayOriginal contem algum dos elementos a serem retirados
+  let novoArray = [];
+
+  arrayFora: for (let i = 0; i < arrayOriginal.length; i++) {
+    for (let j = 0; j < elementosRetirados.length; j++) {
+      if (arrayOriginal[i] === elementosRetirados[j]) continue arrayFora;
+    }
+    novoArray.push(arrayOriginal[i]);
+  }
+  return novoArray;
 };
+
+//Retirar elementos que existem no array de elementos a serem retirados
 
 // Do not edit below this line
 module.exports = removeFromArray;
