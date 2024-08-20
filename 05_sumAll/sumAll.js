a2d4f70083d28f24002ab6e105ff724a904fae21
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-08-20 11:53:46 -0300

    Finish: sumAll all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,14 @@
 const sumAll = function (...numbers) {
   let resultado = 0;
 
+  if (numbers.find((item) => item < 0)) return "ERROR";
+
+  if(numbers.find((item) => !Number.isInteger(item))) return 'ERROR';
+  
   numbers.sort((a, b) => a - b);
   
   numeroInicial = numbers[0];
   numeroFinal = numbers[1];
-
-  if (numbers.find((item) => item < 0)) return "ERROR";
 
   for (let i = numeroInicial; i <= numeroFinal; i++) {
     resultado += i;
