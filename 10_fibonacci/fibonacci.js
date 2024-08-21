0f79a6185c745e6185b0cd4cc9432e1e8bf10e90
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-08-21 14:53:02 -0300

    Finish: fibonnaci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (numero) {
+    if (numero < 0) return "OOPS";
+  let resultado = 0;
+    let sequencia = 1;
+  for (let i = 0; i < +numero; i++) {
+    let aux = resultado;
+    resultado = resultado + sequencia;
+    sequencia = aux;
+  }
+  return resultado;
 };
 
 // Do not edit below this line
