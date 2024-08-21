0cc1c1b3adab8b44c438847e6140f2764ee0a301
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-08-20 21:22:39 -0300

    Finish: palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (palavra) {
+  let palavraCortada = [];
 
+  for (let i = 0; i < palavra.length; i++) {
+    if (palavra.charAt(i).match(/[^a-zA-Z0-9]/)) continue;
+    palavraCortada.push(palavra.charAt(i));
+  }
+
+  let aux = palavraCortada.join("").toLowerCase();
+  let auxReversivel = palavraCortada.reverse().join("").toLowerCase();
+  return aux == auxReversivel ? true : false;
 };
 
 // Do not edit below this line
