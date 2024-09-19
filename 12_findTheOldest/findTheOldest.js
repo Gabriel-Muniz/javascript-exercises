e5adfd657ec0de0ed48967b48cdf72624cd930dc
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-09-19 16:03:04 -0300

    Finish: finTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getAge = function(death, birth) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
+const findTheOldest = function (people) {
+  return people.reduce((oldest, person) => {
+    const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentAge = getAge(person.yearOfDeath, person.yearOfBirth);
+
+    return oldestAge < currentAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
