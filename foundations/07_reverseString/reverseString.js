61e343d2ac258318c166c9d39ab94ff9f2102323
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-07-20 19:25:45 -0300

    Add: finish exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let stringArr = string.split('');
+    let stringReversed = [];
 
+    for (let i = stringArr.length; i >= 0; i--) {
+        stringReversed.push(stringArr[i]);
+    }
+
+    return stringReversed.join('');
 };
 
 // Do not edit below this line
