3d3d3c3e0ca464561f5293c6b9aa31c7140557a3
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-07-20 19:17:39 -0300

    Add: finish exercise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if(num < 0) return "ERROR";
+    
+    let aux = "";
+    for (let i = 0; i < num; i++) {
+        aux += string;
+    }
+
+    return aux;
 };
 
 // Do not edit below this line
