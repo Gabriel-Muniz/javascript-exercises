de7d87b09bac43e924dc8410d1a6edc9cb7ae307
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-07-21 11:10:08 -0300

    Add: finish exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(...args) {
 
+    if(args.find(item => item < 0 || !Number.isInteger(item))){
+        return 'ERROR'
+    }
+
+    args.sort((a, b) => a - b);
+
+    let sum = 0;
+
+    for (let i = args[0]; i <= args[1]; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
