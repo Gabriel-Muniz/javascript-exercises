ff73a92d3ea44d5262fe116348d372045b0f76e1
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-07-21 11:38:34 -0300

    Add: finish exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsiusTemp = (temp - 32) * (5/9);
+  return formatTemp(celsiusTemp);
 };
 
-const convertToFahrenheit = function() {
+const formatTemp = function(temp){
+  return (Number.isInteger(temp)) ? temp : Number(temp.toFixed(1));
+}
+
+const convertToFahrenheit = function(temp) {
+  let fahrenheitTemp = (temp * (9/5)) + 32;
+  return formatTemp(fahrenheitTemp);
 };
 
 // Do not edit below this line
