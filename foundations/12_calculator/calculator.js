55263e5904f513b487bfa907e6585e55f124e580
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-08-12 18:42:06 -0300

    feat: finish exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbersToSum) {
+  return numbersToSum.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbersToMultiply) {
+  return numbersToMultiply.reduce((accumulator, currentValue) => accumulator * currentValue)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+
+  while(num > 0){
+    result *= num;
+
+    num--
+  }
+  
+  return result;
 };
 
 // Do not edit below this line
