b60b42900e9a828cf7823512dcb41f5fc0fc5679
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-08-13 00:24:10 -0300

    feat: finish exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+    const oldestPerson = peopleArray.reduce((oldest, currentPerson) => {
+        let auxOldestAge = ((oldest.yearOfDeath) ? oldest.yearOfDeath : new Date().getFullYear()) - oldest.yearOfBirth;
+        let auxCurrentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
 
+        return (auxOldestAge > auxCurrentAge) ? oldest : currentPerson;
+    }, peopleArray[0])
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
